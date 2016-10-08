4e305fddca5a506576c48352832508f91a39948d
DengXu <xudeng@XudeMacBook-Pro.local>
2016-10-08 11:28:54 +0800

    首次导入项目

---
+++ Calculator/Calculate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calculate
+    {
+        //起步距离2公里，起步价18元，2公里到8公里每公里1元，8公里以后每公里1.2元，等时费每分钟0.5
+        public double theFeeForTaxi(double distance, double time = 0)
+        {
+            double the_fee_for_distance = 0;
+            double the_fee_for_time = time * 0.5;
+
+            if(distance <= 2)
+            {
+                the_fee_for_distance = 18;
+            }else if(distance <= 8)
+            {
+                the_fee_for_distance = 18 + (distance - 2) * 1;
+            }
+            else
+            {
+                the_fee_for_distance = 18 + (8 - 2) * 1 + (distance - 8) * 1.2;
+            }
+
+
+            return the_fee_for_distance + the_fee_for_time;
+        }
+    }
+}
---
+++ CalculatorTest/CalculateTest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Calculator;
+namespace CalculatorTest
+{
+    public class CalculateTest
+    {
+        [Fact]
+        public void shoule_return_18_when_travle_1km()
+        {
+            double distance = 1;
+            Calculator.Calculate calculate = new Calculator.Calculate();
+            double fee = calculate.theFeeForTaxi(distance);
+            Assert.Equal(18, fee);
+        }
+
+        [Fact]
+        public void shoule_return_18_when_travle_2km()
+        {
+            double distance = 1;
+            Calculator.Calculate calculate = new Calculator.Calculate();
+            double fee = calculate.theFeeForTaxi(distance);
+            Assert.Equal(18, fee);
+        }
+
+        [Fact]
+        public void shoule_return_20_when_travle_4km()
+        {
+            double distance = 4;
+            Calculator.Calculate calculate = new Calculator.Calculate();
+            double fee = calculate.theFeeForTaxi(distance);
+            Assert.Equal(20, fee);
+        }
+
+        [Fact]
+        public void shoule_return_24_when_travle_8km()
+        {
+            double distance = 8;
+            Calculator.Calculate calculate = new Calculator.Calculate();
+            double fee = calculate.theFeeForTaxi(distance);
+            Assert.Equal(24, fee);
+        }
+
+        [Fact]
+        public void shoule_return_26_4_when_travle_10km()
+        {
+            double distance = 10;
+            Calculator.Calculate calculate = new Calculator.Calculate();
+            double fee = calculate.theFeeForTaxi(distance);
+            Assert.Equal(26.4, fee);
+        }
+
+        [Fact]
+        public void shoule_return_28_9_when_travle_10km_and_wait_5min()
+        {
+            double distance = 10;
+            double time = 5;
+            Calculator.Calculate calculate = new Calculator.Calculate();
+            double fee = calculate.theFeeForTaxi(distance,time);
+            Assert.Equal(28.9, fee);
+        }
+    }
+}
